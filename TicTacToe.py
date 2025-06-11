51cd335ab261075972997f675191931a707b4c0f
Lapin <suicidatore@gmail.com>
2025-06-11 21:53:43 +0000

    probléme de chunks de chunks

--- TicTacToe.py
+++ TicTacToe.py
@@ -33,23 +33,38 @@ def creadico():
 creadico()
 print(dico)
 
+visuelF=[" "]
 
 #on refait la fonction ci dessus en un print:
 #on va créer une liste (visuelF) qui nous serviras a print seulement la notre rendu final grace a visuel qui est une liste un peu plus graphique de l'etat de notre dico
+
+#pas sur d'avoir compris thaumaturge ce que je viens de copier dans les 2 lignes du dessous
+def split_list(lst, chunk_size):
+    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+
 def Affichage(dico):
     for index,fruit in enumerate(dico.values()):
-            if fruit==0:
-                visuel[index]=" "
-            if fruit<0:
-                visuel[index]="X"
-            if fruit>0:
-                visuel[index]="O"
-    print(" ", line[0], line[1], line[2])
-    print(line[0], visuel[0], visuel[1], visuel[2])
-    print(line[1], visuel[3], visuel[4], visuel[5])
-    print(line[2], visuel[6], visuel[7], visuel[8])
+        if fruit==0:
+            visuel[index]=" "
+        if fruit<0:
+            visuel[index]="X"
+        if fruit>0:
+            visuel[index]="O"
+    chunks = split_list(visuel, taille)
+    for i in range(taille):
+        visuelF.append(colonne[i])
+    for i in range(taille):
+        visuelF.append(ligne[i])
+        visuelF.append(chunks[i])
+    chunks2 = split_list(visuelF, (taille+1))
+    for i in range(0, (taille+1)):
+        print(chunks2[i],"\n")
+Affichage(dico)
 
-###Affichage(dico)
+#print(" ", line[0], line[1], line[2])
+#    print(line[0], visuel[0], visuel[1], visuel[2])
+#    print(line[1], visuel[3], visuel[4], visuel[5])
+#    print(line[2], visuel[6], visuel[7], visuel[8])
 
 #print("\n",case['AA'])
 #fruits = ['pomme', 'banane', 'orange']
