ea60929687ab27f349c01f40a8456cc1c7691326
Lapin <suicidatore@gmail.com>
2025-06-12 07:04:51 +0000

    probléme de chunk réglé, mais la fin buggé

--- TicTacToe.py
+++ TicTacToe.py
@@ -35,9 +35,6 @@ print(dico)
 
 visuelF=[" "]
 
-#on refait la fonction ci dessus en un print:
-#on va créer une liste (visuelF) qui nous serviras a print seulement la notre rendu final grace a visuel qui est une liste un peu plus graphique de l'etat de notre dico
-
 #pas sur d'avoir compris thaumaturge ce que je viens de copier dans les 2 lignes du dessous
 def split_list(lst, chunk_size):
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
@@ -56,18 +53,16 @@ def Affichage(dico):
     for i in range(taille):
         visuelF.append(ligne[i])
         visuelF.append(chunks[i])
-    chunks2 = split_list(visuelF, (taille+1))
-    for i in range(0, (taille+1)):
-        print(chunks2[i],"\n")
+    print(visuelF[:(taille+1)],"\n")
+    for i in range(1, (taille-1)):
+        print(visuelF[(taille+i):(taille+i+2)],"\n")
+        print(taille+i)
+        print(taille+i+2)
 Affichage(dico)
 
-#print(" ", line[0], line[1], line[2])
-#    print(line[0], visuel[0], visuel[1], visuel[2])
-#    print(line[1], visuel[3], visuel[4], visuel[5])
-#    print(line[2], visuel[6], visuel[7], visuel[8])
+# ligne 60, 61, 62, 63 n'apparaisse pas ??
+# ligne 62 et 63 sont la pour voir ce qui cloche
 
-#print("\n",case['AA'])
 #fruits = ['pomme', 'banane', 'orange']
 #for index, fruit in enumerate(fruits):
 #    print(index, fruit)
-# utiliser un modulot pour rajouter un "\n" dans les printX4 pour automatiser le truc (le modulo depends de la taille du plateau, demander la taille du plateau au debut)
