9f69be7118b52645f146e05f49fb38775f6f24cb
Lapin <suicidatore@gmail.com>
2025-06-13 18:32:50 +0000

    Fonctions winCondition, fTour, Affichage() finies

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,15 +8,24 @@
 #
 #ces commandes permettent de mettre a jour le github 
 
+#-----------------------------------------------------Block init & variable-----------------------------------------------------
+sumListeWin=[]
+listeWin=[]
+listesC=[]
+listesL=[]
+listeD_0=[]
+listeD_1=[]
+visuelF=[" "]
+tour=0
+
 taille=int(input('donner la taille de votre morpion ?'))
 colonne=[chr(i+65)for i in range(taille*2)]
 colonne
 ligne=colonne[int(taille):]
 colonne = colonne[:int(taille)]
-#print(colonne)         ##################
-#for i in ligne:
-#   print(i)
-
+#print(colonne)              ##################               
+#for i in ligne:            ##################
+#   print(i)                ##################
 tailleSquare=taille**2
 visuel=[0 for i in range(tailleSquare)]
 dico={}
@@ -29,8 +38,9 @@ def creadico():
         dico[listeindex[i]]=0
 creadico()
 #print(dico)         ##################
+liste_cles=list(dico.keys())    #pour faire appelle aux valeurs du dico par index et non par clé
 
-visuelF=[" "]
+
 
 def split_list(lst, chunk_size):
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
@@ -38,17 +48,21 @@ def split_list(lst, chunk_size):
 ########TEST############
 dico={"AD":0,
       "BD":-1,
-      "CD":3,
-      "AE":-2,
-      "BE":5,
-      "CE":-6,
-      "AF":9,
-      "BF":155,
-      "CF":2
+      "CD":1,
+      "AE":-1,
+      "BE":1,
+      "CE":-1,
+      "AF":1,
+      "BF":0,
+      "CF":1
       }
 print(dico)
 #############TEST############
+#----------------------------------------------------------Block tour-----------------------------------------------------
+def fTour():
+    tour=tour+1
 
+#---------------------------------------------------------Block affichage-----------------------------------------------------
 def Affichage(dico):
     for index,fruit in enumerate(dico.values()):
         if fruit==0:
@@ -68,15 +82,7 @@ def Affichage(dico):
         print(visuelF[(taille+i):(taille+i+2)],"\n")
 Affichage(dico)
 
-
-
-liste_cles=list(dico.keys())    #pour faire appelle aux valeurs du dico par index et non par clé
-
-
-listesC=[]
-listesL=[]
-listeD_0=[]
-listeD_1=[]
+#-----------------------------------------------------Block winCondition-----------------------------------------------------
 for i in range(taille):
     listesC.append([])   #on ajoute une nouvelle liste vide à chaque itération
     for y in range(i, taille*(taille-1)+i+1, taille):
@@ -91,27 +97,40 @@ for y in range(taille):
     listeD_0.append(dico[liste_cles[(y+1)*(taille-1)]])
 for y in range(taille):
     listeD_1.append(dico[liste_cles[y*(taille+1)]])
-
-for i, listec in enumerate(listesC):                ##########
-    print(f"ListeC {i} : {listec}")
-for i, listel in enumerate(listesL):                ##########
-    print(f"ListeL {i} : {listel}")
-print(listeD_0)
-print(listeD_1)
-
-# winCondition : fonctionnement "3 en ligne, 3 en column, ou 3 en diagonal => win /// if not => draw"
-
-
-# toursSysteme : fonctionnement "on commence a un tour et on repette jusqu'a ce que winCondition reagisse, si winCondition == true => fin du compteur de tour,
-#                   reinitialisation du compteur de tour, de dico, de visuel, visuelF et de wincond, ET demandez "voulez vous rejouer"
+#for i, listec in enumerate(listesC):                ##########
+#    print(f"ListeC {i} : {listec}")                 ##########
+#for i, listel in enumerate(listesL):                ##########
+#    print(f"ListeL {i} : {listel}")                 ##########
+#print("ListeD_0 :", listeD_0)                       ##########
+#print("ListeD_1 :", listeD_1)                       ##########
+for i in range(len(listesC)):
+    listeWin.append(listesC[i])
+for i in range(len(listesL)):
+    listeWin.append(listesL[i])
+listeWin.append(listeD_0)
+listeWin.append(listeD_1)
+#print(listeWin)                                    ##########
+sumListeWin=[sum(sous_liste) for sous_liste in listeWin]
+#print(sumListeWin)                                 ##########
+def winCondition():
+    #Draw
+    if fTour == tailleSquare:
+        print(f"Draw, {fTour} have been played")
+        return 1
+    #Win
+    for i in range(len(sumListeWin)):
+        if sumListeWin[i]==taille:
+            print("Player 2 won")
+            return 1
+        if sumListeWin[i]==(-taille):
+            print("Player 1 won")
+            return 1
+    return 0
 
 # jouez : fonctionnement "demander au joueur x de jouer /// if wrong input => message error ET joueur x rejoue /// implémenter l'index jouer par joueur X avec -1 ET passez au joueur O
 #               demander a joueur O de jouer /// error message too /// impl l'index jouer par joueur O avec +1 ET passe au joueur suivant" Tant que windcond =/= 0
 
-# Et c'est fini
 
+# trouver un moyen d'actualiser toutes les listes qui finnissent par créer la listeWin
 
-
-#fruits = ['pomme', 'banane', 'orange']
-#for index, fruit in enumerate(fruits):
-#    print(index, fruit)
+# demandez "voulez vous rejouer" (if wincond == 1 => ...)
