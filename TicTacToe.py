836932d1f1213792b4122b1fa30df31685bd9149
Lapin <suicidatore@gmail.com>
2025-06-13 20:15:12 +0000

    Almost done, a really good "donner la taille de votre morpion?" printer !

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,15 +17,21 @@ listeD_0=[]
 listeD_1=[]
 visuelF=[" "]
 tour=0
+pTour=2
+alreadyPlayed=[]
+wrongK=0
+p1=-1
+p2=+1
+player=0
 
 taille=int(input('donner la taille de votre morpion ?'))
 colonne=[chr(i+65)for i in range(taille*2)]
 colonne
 ligne=colonne[int(taille):]
 colonne = colonne[:int(taille)]
-#print(colonne)              ##################               
-#for i in ligne:            ##################
-#   print(i)                ##################
+#print(colonne)                                         ##########              
+#for i in ligne:                                        ##########
+#   print(i)                                            ##########
 tailleSquare=taille**2
 visuel=[0 for i in range(tailleSquare)]
 dico={}
@@ -37,7 +43,7 @@ def creadico():
     for i in range(len(listeindex)):
         dico[listeindex[i]]=0
 creadico()
-#print(dico)         ##################
+#print(dico)                                            ##########
 liste_cles=list(dico.keys())    #pour faire appelle aux valeurs du dico par index et non par clé
 
 
@@ -45,22 +51,28 @@ liste_cles=list(dico.keys())    #pour faire appelle aux valeurs du dico par inde
 def split_list(lst, chunk_size):
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
 
-########TEST############
-dico={"AD":0,
-      "BD":-1,
-      "CD":1,
-      "AE":-1,
-      "BE":1,
-      "CE":-1,
-      "AF":1,
-      "BF":0,
-      "CF":1
-      }
-print(dico)
-#############TEST############
+#dico={"AD":0,                                          ##########
+#      "BD":-1,                                         ##########
+#      "CD":1,                                          ##########
+#      "AE":-1,                                         ##########
+#      "BE":1,                                          ##########
+#      "CE":-1,                                         ##########
+#      "AF":1,                                          ##########
+#      "BF":0,                                          ##########
+#      "CF":1                                           ##########
+#      }                                                ##########
+#print(dico)                                            ##########
 #----------------------------------------------------------Block tour-----------------------------------------------------
 def fTour():
     tour=tour+1
+    if tour % 2 == 0:
+        return 0
+    if tour % 2 != 0:
+        return 1
+if fTour == 1:
+    ptour=1
+if fTour == 0:
+    ptour=2
 
 #---------------------------------------------------------Block affichage-----------------------------------------------------
 def Affichage(dico):
@@ -80,7 +92,7 @@ def Affichage(dico):
     print(visuelF[:(taille+1)],"\n")
     for i in range(1, (taille*2), 2):
         print(visuelF[(taille+i):(taille+i+2)],"\n")
-Affichage(dico)
+#Affichage(dico)                                     ##########
 
 #-----------------------------------------------------Block winCondition-----------------------------------------------------
 for i in range(taille):
@@ -109,9 +121,9 @@ for i in range(len(listesL)):
     listeWin.append(listesL[i])
 listeWin.append(listeD_0)
 listeWin.append(listeD_1)
-#print(listeWin)                                    ##########
+#print(listeWin)                                     ##########
 sumListeWin=[sum(sous_liste) for sous_liste in listeWin]
-#print(sumListeWin)                                 ##########
+#print(sumListeWin)                                  ##########
 def winCondition():
     #Draw
     if fTour == tailleSquare:
@@ -127,10 +139,31 @@ def winCondition():
             return 1
     return 0
 
+#-----------------------------------------------------Block Playing-----------------------------------------------------
+def playme(input:f"Player {pTour} chooses a play please"):
+    Affichage
+    if pTour==1:
+        player=int(p1)
+    if pTour==2:
+        player=int(p2)
+    if dico.get(input) == None:
+        print("Wrong keys try something like AF")
+        return wrongK==1
+    if alreadyPlayed.get(input):
+        print("Already played this combinaison, try again")
+        return wrongK==1
+    if wrongK==0:
+        alreadyPlayed.append(input)
+        winCondition
+        fTour
+        dico[input]=player
+
+def run1():
+    while winCondition==0:
+        playme
+        wrongK==0
+run1
 # jouez : fonctionnement "demander au joueur x de jouer /// if wrong input => message error ET joueur x rejoue /// implémenter l'index jouer par joueur X avec -1 ET passez au joueur O
 #               demander a joueur O de jouer /// error message too /// impl l'index jouer par joueur O avec +1 ET passe au joueur suivant" Tant que windcond =/= 0
-
-
-# trouver un moyen d'actualiser toutes les listes qui finnissent par créer la listeWin
 
 # demandez "voulez vous rejouer" (if wincond == 1 => ...)
